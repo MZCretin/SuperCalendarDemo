0aaa6919b066d4e7d6d8e8643c7494b3859c9512
CretinZp <mxnzp_life@163.com>
2018-07-27 19:20:27 +0800

    bug 修复

--- app/src/main/java/com/cretin/www/calendarviewdemo/adapter/CalendarPagerAdapter.java
+++ app/src/main/java/com/cretin/www/calendarviewdemo/adapter/CalendarPagerAdapter.java
@@ -1,4 +1,4 @@
-package com.cretin.www.calendarviewdemo;
+package com.cretin.www.calendarviewdemo.adapter;
 
 import android.content.Context;
 import android.graphics.Color;
@@ -11,20 +11,34 @@ import android.view.ViewGroup;
 import com.cretin.www.calendarviewdemo.views.CalendarView;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class CalendarPagerAdapter extends PagerAdapter {
-
     //缓存上一次回收的MonthView
     private LinkedList<CalendarView> cache = new LinkedList<>();
-    private SparseArray<CalendarView> mViews = new SparseArray<>();
 
-    private Context context;
+    //记录当前展示的View
+    private CalendarView currView;
 
+    //记录需要展示的数量
     private int count;
 
-    public CalendarPagerAdapter(int count, Context context) {
+    //初始化事件监听
+    private CalendarView.OnItemClickListener listener;
+
+    public CalendarPagerAdapter(int count, CalendarView.OnItemClickListener listener) {
         this.count = count;
-        this.context = context;
+        this.listener = listener;
+    }
+
+    public void setHuiKuan(List<String> dates) {
+        if (currView != null)
+            currView.setHuiKuan(dates);
+    }
+
+    @Override
+    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+        currView = (CalendarView) object;
     }
 
     @Override
@@ -39,28 +53,31 @@ public class CalendarPagerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
+        //尽可能复用View
         final CalendarView view;
         if (!cache.isEmpty()) {
             view = cache.removeFirst();
+            //根据position获取当前页面需要展示的年份和月份
             int[] yearAndMonth = getYearAndMonth(position);
             view.setData(yearAndMonth[0], yearAndMonth[1]);
-            Log.e("HHHHHHH", "gangal   " + view.getChildCount() + "   " + view.getHeight());
         } else {
             view = new CalendarView(container.getContext());
-            view.setBackgroundResource(R.drawable.bg);
+            //根据position获取当前页面需要展示的年份和月份
             int[] yearAndMonth = getYearAndMonth(position);
-            view.setInitData(yearAndMonth[0], yearAndMonth[1]);
-            Log.e("HHHHHHH", "gangal1   " + view.getChildCount() + "   " + view.getHeight());
+            view.setData(yearAndMonth[0], yearAndMonth[1]);
         }
-        mViews.put(position, view);
+        if (listener != null)
+            view.setListener(listener);
         container.addView(view);
         return view;
     }
 
+    //记录开始的年份 我们这里是从2014-5 到 这个月后面的36个月
     int startYear = 2014;
     int startMonth = 5;
     int afterMonth = 36;
 
+    //根据position获取到此页面需要展示年月份的数据
     public int[] getYearAndMonth(int position) {
         int cMonth = startMonth + position;
         int cYear = startYear + cMonth / 12;
@@ -78,6 +95,7 @@ public class CalendarPagerAdapter extends PagerAdapter {
         return new int[]{cYear, cMonth};
     }
 
+    //根据年月反推position
     public int getPosition(int[] yearAndMonth) {
         int year = yearAndMonth[0];
         int month = yearAndMonth[1];
@@ -92,20 +110,9 @@ public class CalendarPagerAdapter extends PagerAdapter {
         return (year - startYear - 1) * 12 + (12 - startMonth) + month;
     }
 
-
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((CalendarView) object);
         cache.addLast((CalendarView) object);
-        mViews.remove(position);
-    }
-
-    /**
-     * 获得ViewPager缓存的View
-     *
-     * @return
-     */
-    public SparseArray<CalendarView> getViews() {
-        return mViews;
     }
 }
--- app/src/main/java/com/cretin/www/calendarviewdemo/views/CalendarView.java
+++ app/src/main/java/com/cretin/www/calendarviewdemo/views/CalendarView.java
@@ -1,31 +1,26 @@
 package com.cretin.www.calendarviewdemo.views;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Color;
-import android.os.Build;
-import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cretin.www.calendarviewdemo.R;
-import com.cretin.www.calendarviewdemo.SquareTextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by cretin on 16/9/21.
  */
-public class CalendarView extends LinearLayout {
+public class CalendarView extends LinearLayout implements View.OnClickListener {
     private Context mContext;
     private String[] weeks = new String[]{"日", "一", "二", "三", "四", "五", "六"};
     private int firstDay = 1;
@@ -36,9 +31,23 @@ public class CalendarView extends LinearLayout {
     private int mWeekColor = Color.parseColor("#AEEC8B");
     private int mDaysColor = Color.parseColor("#1d1d26");
 
+    private OnItemClickListener listener;
+
     private int defaultWidth = 100;
 
+    private float scaleSize;
+
     private LinearLayout[][] containers = new LinearLayout[6][7];
+
+    //目标年份 目标月份 和 目标日期
+    private int aimYear;
+    private int aimMonth;
+    private int aimDay;
+
+    //今日年份 今日月份 和 今日日期
+    private int tYear;
+    private int tMonth;
+    private int tDay;
 
     //创建一个二维数组来存放显示日期的控件
     private TextView[][] days = new TextView[6][7];
@@ -48,10 +57,13 @@ public class CalendarView extends LinearLayout {
     //记录存放日期控件的LinearLayout
     private LinearLayout[] layouts = new LinearLayout[6];
 
-    //是否初始化
-    private boolean initFlag;
-    //容器宽度
-    private int mPanelWidth;
+    public OnItemClickListener getListener() {
+        return listener;
+    }
+
+    public void setListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
 
     public CalendarView(Context context) {
         super(context);
@@ -71,11 +83,20 @@ public class CalendarView extends LinearLayout {
     //初始化
     private void init(Context context) {
         mContext = context;
+        scaleSize = getScaleSize();
+        defaultWidth = (int) (scaleSize * 26);
+
         setOrientation(VERTICAL);
+
+        initHeadView(mContext);
+
+        initBodyView(mContext);
+
+        putData(dates);
     }
 
     //填充数据
-    private void putData() {
+    private void putData(List<String> dates) {
         int lastMonthLastDay = getLastDayOfMonth(currYear, currMonth);
         int firstDayOfMonth = getFisrtDayOfMonth(currYear, currMonth);
         int daysOfMonth = getDaysOfMonth(currYear, currMonth);
@@ -86,8 +107,25 @@ public class CalendarView extends LinearLayout {
                 if (i == 0) {
                     if (j >= firstDayOfMonth - 1) {
                         days[i][j].setText((firstDay++) + "");
+
+                        //设置回款
+                        setHuikuanFlag(dates, days[i][j], i, j);
+
+                        //设置当前选中日期背景
+                        if (currYear == aimYear && currMonth == aimMonth && Integer.toString(aimDay).equals(days[i][j].getText().toString())) {
+                            days[i][j].setBackgroundResource(R.mipmap.day_bg);
+                            days[i][j].setTextColor(Color.parseColor("#79B856"));
+                        } else {
+                            days[i][j].setTextColor(Color.parseColor("#FFFFFF"));
+
+                            //设置今日时间颜色
+                            if (currYear == tYear && currMonth == tMonth && Integer.toString(tDay).equals(days[i][j].getText().toString())) {
+                                days[i][j].setTextColor(Color.parseColor("#E9F91F"));
+                            }
+                        }
+
+                        containers[i][j].setTag(currYear + "-" + currMonth + "-" + days[i][j].getText().toString());
                         temp++;
-                        days[i][j].setTextColor(Color.parseColor("#FFFFFF"));
                     } else {
                         //上个月的日期
                         days[i][j].setText((lastMonthLastDay - firstDayOfMonth + 2 + j) + "");
@@ -96,11 +134,29 @@ public class CalendarView extends LinearLayout {
                 } else {
                     if (firstDay <= daysOfMonth) {
                         days[i][j].setText((firstDay++) + "");
-                        days[i][j].setTextColor(Color.parseColor("#FFFFFF"));
+
+                        //设置回款
+                        setHuikuanFlag(dates, days[i][j], i, j);
+
+                        //设置当前选中日期背景
+                        if (currYear == aimYear && currMonth == aimMonth && Integer.toString(aimDay).equals(days[i][j].getText().toString())) {
+                            days[i][j].setBackgroundResource(R.mipmap.day_bg);
+                            days[i][j].setTextColor(Color.parseColor("#79B856"));
+                        } else {
+                            days[i][j].setTextColor(Color.parseColor("#FFFFFF"));
+
+                            //设置今日时间颜色
+                            if (currYear == tYear && currMonth == tMonth && Integer.toString(tDay).equals(days[i][j].getText().toString())) {
+                                days[i][j].setTextColor(Color.parseColor("#E9F91F"));
+                            }
+                        }
+
+                        containers[i][j].setTag(currYear + "-" + currMonth + "-" + days[i][j].getText().toString());
                         if (i < 5) {
                             temp++;
                         }
                     } else {
+                        //下个月的日期
                         days[i][j].setText((index++) + "");
                         days[i][j].setTextColor(Color.parseColor("#AEEC8B"));
                     }
@@ -114,54 +170,31 @@ public class CalendarView extends LinearLayout {
             //说明5行显示完了 隐藏第6行
             layouts[5].setVisibility(GONE);
         }
-//        drawOval();
-        huikuan[0][0].setText(currYear + "-" + currMonth);
     }
 
-    private void drawOval() {
-        int firstDayOfMonth = getFisrtDayOfMonth(currYear, currMonth);
-        int daysOfMonth = getDaysOfMonth(currYear, currMonth);
-
-        Date dt = new Date();
-        SimpleDateFormat matter = new SimpleDateFormat("yyyy MM dd");
-        int year = Integer.parseInt(matter.format(dt).split(" ")[0]);
-        int month = Integer.parseInt(matter.format(dt).split(" ")[1]);
-        int day = Integer.parseInt(matter.format(dt).split(" ")[2]);
-
-        if (currYear == year && month == currMonth) {
-            HH:
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 7; j++) {
-                    if (i == 0) {
-                        if (j >= firstDayOfMonth - 1) {
-                            if (firstDayCopy == day && month == currMonth && year == currYear) {
-                                days[i][j].setBackgroundResource(R.mipmap.day_bg);
-                                days[i][j].setTextColor(Color.WHITE);
-                                firstDayCopy = 1;
-                                break HH;
-                            }
-                            firstDayCopy++;
-                        }
-                    } else {
-                        if (firstDayCopy <= daysOfMonth) {
-                            if (firstDayCopy == day && month == currMonth && year == currYear) {
-                                days[i][j].setBackgroundResource(R.mipmap.day_bg);
-                                days[i][j].setTextColor(Color.WHITE);
-                                firstDayCopy = 1;
-                                break HH;
-                            }
-                            firstDayCopy++;
-                        }
+    //设置回款的flag
+    private void setHuikuanFlag(List<String> dates, TextView textView, int i, int j) {
+        //设置回款标识
+        if (dates != null && !dates.isEmpty())
+            for (String str :
+                    dates) {
+                int year = Integer.parseInt(str.split("-")[0]);
+                int month = Integer.parseInt(str.split("-")[1]);
+                int day = Integer.parseInt(str.split("-")[2]);
+                if (currYear == year && currMonth == month && Integer.toString(day).equals(textView.getText().toString())) {
+                    textView.setBackgroundResource(R.mipmap.quan);
+                    if (huikuan[i][j] != null) {
+                        huikuan[i][j].setText("回款");
                     }
                 }
             }
-        }
     }
 
     //初始化整体布局
     private void initBodyView(Context context) {
-        int margin = (mPanelWidth - 7 * defaultWidth) / 14;
+        int margin = (int) (10 * scaleSize);
         setPadding(0, 20, 0, 50 - margin);
+        //添加日期的数据 6行
         for (int i = 0; i < 6; i++) {
             layouts[i] = new LinearLayout(context);
             layouts[i].setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1));
@@ -175,34 +208,32 @@ public class CalendarView extends LinearLayout {
 
                 days[i][j] = new TextView(context);
 
-                LayoutParams lpWeek = new LayoutParams(defaultWidth, defaultWidth);
+                LayoutParams lpWeek = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 lpWeek.weight = 1;
                 lpWeek.setMargins(margin, margin, margin, margin);
 
-                days[i][j].setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                days[i][j].setLayoutParams(new LayoutParams(defaultWidth, defaultWidth));
                 days[i][j].setTextColor(mDaysColor);
                 days[i][j].setTextSize(14);
                 days[i][j].setGravity(Gravity.CENTER);
-//                days[i][j].setBackgroundResource(R.mipmap.day_bg);
 
                 containers[i][j].addView(days[i][j]);
 
                 LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                layoutParams.setMargins(0, 10, 0, 0);
+                layoutParams.setMargins(0, (int) (2 * scaleSize), 0, 0);
                 huikuan[i][j] = new TextView(context);
                 huikuan[i][j].setLayoutParams(layoutParams);
                 huikuan[i][j].setTextColor(Color.parseColor("#F8E71C"));
                 huikuan[i][j].setTextSize(10);
                 huikuan[i][j].setGravity(Gravity.CENTER);
-                huikuan[i][j].setText("回款");
 
                 containers[i][j].addView(huikuan[i][j]);
 
+                containers[i][j].setOnClickListener(this);
                 layouts[i].addView(containers[i][j]);
             }
             addView(layouts[i]);
         }
-        initFlag = true;
     }
 
     //初始化头部布局
@@ -211,16 +242,17 @@ public class CalendarView extends LinearLayout {
         Date dt = new Date();
         SimpleDateFormat matter = new SimpleDateFormat("yyyy MM dd");
         if (currYear == 0)
-            currYear = Integer.parseInt(matter.format(dt).split(" ")[0]);
+            tYear = currYear = Integer.parseInt(matter.format(dt).split(" ")[0]);
         if (currMonth == 0)
-            currMonth = Integer.parseInt(matter.format(dt).split(" ")[1]);
-        currDay = Integer.parseInt(matter.format(dt).split(" ")[2]);
+            tMonth = currMonth = Integer.parseInt(matter.format(dt).split(" ")[1]);
+        tDay = currDay = Integer.parseInt(matter.format(dt).split(" ")[2]);
 
+        //添加头部的显示星期的布局
         LinearLayout llWeek = new LinearLayout(context);
-        llWeek.setPadding(0, 0, 0, 0);
         llWeek.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         llWeek.setOrientation(HORIZONTAL);
         for (int i = 0; i < 7; i++) {
+            //把最终显示星期的TextView添加到LinearLayout里面去
             TextView week = new TextView(context);
             LayoutParams lpWeek = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             lpWeek.weight = 1;
@@ -231,73 +263,35 @@ public class CalendarView extends LinearLayout {
             week.setGravity(Gravity.CENTER);
             llWeek.addView(week, lpWeek);
         }
+        //将星期添加到视图中
         addView(llWeek);
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        if (mPanelWidth == 0) {
-            int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-            int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-
-            int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-            int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-
-            int width = Math.min(widthSize, heightSize);
-            if (widthMode == MeasureSpec.UNSPECIFIED) {
-                width = heightSize;
-            } else if (heightMode == MeasureSpec.UNSPECIFIED) {
-                width = widthSize;
-            }
-            mPanelWidth = width;
-
-            initHeadView(mContext);
-
-            initBodyView(mContext);
-
-            putData();
-        } else {
-            Log.e("HHH", "kepa");
-        }
-    }
-
-
     //清除数据
     private void clearData() {
+        //清除所有状态
         firstDay = 1;
         firstDayCopy = 1;
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
                 days[i][j].setText("");
+                containers[i][j].setTag(null);
                 days[i][j].setBackgroundDrawable(null);
                 days[i][j].setTextColor(mDaysColor);
+                huikuan[i][j].setText("");
             }
         }
     }
 
-    public void setInitData(int year, int month) {
+    public void setData(int year, int month) {
+        clearData();
         if (month <= 0 || month > 12)
             throw new RuntimeException("month 数据有误");
         if (year < 1970)
             throw new RuntimeException("year 数据有误");
         currMonth = month;
         currYear = year;
-    }
-
-    public void setData(int year, int month) {
-        if (initFlag) {
-            clearData();
-            if (month <= 0 || month > 12)
-                throw new RuntimeException("month 数据有误");
-            if (year < 1970)
-                throw new RuntimeException("year 数据有误");
-            currMonth = month;
-            currYear = year;
-            putData();
-        } else {
-            Log.e("HHHHHHH", "大事不好");
-        }
+        putData(dates);
     }
 
     //获取指定年份指定月份的第一天的位置
@@ -339,5 +333,38 @@ public class CalendarView extends LinearLayout {
         cal.set(Calendar.MONTH, month);
         cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DATE));
         return Integer.parseInt(new SimpleDateFormat("dd").format(cal.getTime()));
+    }
+
+    private float getScaleSize() {
+        TextView textView = new TextView(mContext);
+        textView.setTextSize(1);
+        return textView.getTextSize();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener != null && view.getTag() != null) {
+            //将目标日期获取用于绘制背景
+            String s = view.getTag().toString();
+            aimYear = Integer.parseInt(s.split("-")[0]);
+            aimMonth = Integer.parseInt(s.split("-")[1]);
+            aimDay = Integer.parseInt(s.split("-")[2]);
+            clearData();
+            putData(dates);
+            listener.onItemClick(view, s);
+        }
+    }
+
+    //数据的格式是 yyyy-MM-dd 如 2016-06-21
+    private List<String> dates;
+
+    public void setHuiKuan(List<String> dates) {
+        this.dates = dates;
+        clearData();
+        putData(dates);
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, String date);
     }
 }
