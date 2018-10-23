3c44a8bd170ccad76249fc1d078ec4f119ea1a8e
MZCretin <mxnzp_life@163.com>
2018-10-23 18:30:09 +0800

    upload

--- app/src/main/java/com/cretin/www/calendarviewdemo/Test.java
+++ app/src/main/java/com/cretin/www/calendarviewdemo/Test.java
@@ -2,6 +2,8 @@ package com.cretin.www.calendarviewdemo;
 
 import android.util.Log;
 
+import static android.content.ContentValues.TAG;
+
 /**
  * @date: on 10/23/18
  * @author: cretin
@@ -10,5 +12,8 @@ import android.util.Log;
  */
 public class Test {
     private void init() {
+        Log.d(TAG, "init: ");
+        Log.d(TAG, "init: ");
+        Log.d(TAG, "init: ");
     }
 }
