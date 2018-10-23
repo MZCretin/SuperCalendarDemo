3fed25a02c1fcb70b925341d28f3df2c5c6575b4
MZCretin <mxnzp_life@163.com>
2018-10-23 18:15:49 +0800

    upload

--- app/src/main/java/com/cretin/www/calendarviewdemo/Test5.java
+++ app/src/main/java/com/cretin/www/calendarviewdemo/Test5.java
@@ -1,15 +1,10 @@
 package com.cretin.www.calendarviewdemo;
 
-import android.util.Log;
-
 /**
  * @date: on 10/23/18
  * @author: cretin
  * @email: mxnzp_life@163.com
  * @desc: 添加描述
  */
-public class Test {
-    private void init() {
-        Log.e("", "");
-    }
+public class Test5 {
 }
